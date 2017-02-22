e21f7edfac08d3a234e3c77afa470693a1537c6f
Alexey Lunev <alu@sitecore.net>
2017-02-22 17:33:55 +0200

    The patch not worked for domain mathcers that had the following rule configured: "where request domain is". That was fixed.

--- src/Sitecore.Support.101816/RequestRuleContext.cs
+++ src/Sitecore.Support.101816/RequestRuleContext.cs
@@ -1,55 +1,65 @@
 namespace Sitecore.Support.FXM.Rules.Contexts
 {
-    using Sitecore.FXM.Rules.Contexts;
-    using System;
-    using System.Runtime.CompilerServices;
-    using System.Web;
+  using Sitecore.FXM.Rules.Contexts;
+  using System;
+  using System.Reflection;
+  using System.Runtime.CompilerServices;
+  using System.Web;
 
-    public class RequestRuleContext : Sitecore.FXM.Rules.Contexts.RequestRuleContext
+  public class RequestRuleContext : Sitecore.FXM.Rules.Contexts.RequestRuleContext
+  {
+    private Uri url;
+
+    public RequestRuleContext(HttpRequestBase request) : base(request)
     {
-        private Uri url;
-
-        public RequestRuleContext(HttpRequestBase request) : base(request)
-        {
-            this.Request = request;
-        }
-
-        public HttpRequestBase Request { get; protected set; }
-
-        public Uri Url
-        {
-            get
-            {
-                if ((this.url == null) && (this.Request.QueryString != null))
-                {
-                    string str = this.Request.QueryString["url"];
-                    if (!string.IsNullOrWhiteSpace(str))
-                    {
-                        try
-                        {
-                            this.url = new Uri(str);
-                        }
-                        catch
-                        {
-                        }
-                    }
-                    else
-                    {
-                        str = this.Request.QueryString["page"];
-                        if (!string.IsNullOrWhiteSpace(str))
-                        {
-                            try
-                            {
-                                this.url = new Uri(str);
-                            }
-                            catch
-                            {
-                            }
-                        }
-                    }
-                }
-                return (this.url ?? (this.url = this.Request.Url));
-            }
-        }
+      this.Request = request;
+      url = Url;
+      AssignBaseClassUrl(url);
     }
+
+    public HttpRequestBase Request { get; protected set; }
+
+    public Uri Url
+    {
+      get
+      {
+        if ((this.url == null) && (this.Request.QueryString != null))
+        {
+          string str = this.Request.QueryString["url"];
+          if (!string.IsNullOrWhiteSpace(str))
+          {
+            try
+            {
+              this.url = new Uri(str);
+            }
+            catch
+            {
+            }
+          }
+          else
+          {
+            str = this.Request.QueryString["page"];
+            if (!string.IsNullOrWhiteSpace(str))
+            {
+              try
+              {
+                this.url = new Uri(str);
+              }
+              catch
+              {
+              }
+            }
+          }
+        }
+        return (this.url ?? (this.url = this.Request.Url));
+      }
+    }
+
+    protected void AssignBaseClassUrl(Uri urlToAssign)
+    {
+      FieldInfo fi = typeof(Sitecore.FXM.Rules.Contexts.RequestRuleContext).GetField("url", BindingFlags.Instance | BindingFlags.NonPublic);
+      fi.SetValue(((Sitecore.FXM.Rules.Contexts.RequestRuleContext)this), urlToAssign);
+      
+    }
+  }
 }
--- src/Sitecore.Support.101816/ValidateByDomainRuleProcessor.cs
+++ src/Sitecore.Support.101816/ValidateByDomainRuleProcessor.cs
@@ -1,36 +1,43 @@
 namespace Sitecore.Support.FXM.Pipelines.Tracking.ValidateRequest
 {
-    using Sitecore.Abstractions;
-    using Sitecore.FXM.Abstractions;
-    using Sitecore.FXM.Matchers;
-    using Sitecore.FXM.Pipelines.Tracking.ValidateRequest;
-    using Sitecore.FXM.Rules.Contexts;
-    using Sitecore.Support.FXM.Rules.Contexts;
-    using System;
-    using System.Linq;
+  using Sitecore.Abstractions;
+  using Sitecore.FXM.Abstractions;
+  using Sitecore.FXM.Matchers;
+  using Sitecore.FXM.Pipelines.Tracking.ValidateRequest;
+  using Sitecore.FXM.Rules.Contexts;
+  using Sitecore.Rules;
+  using Sitecore.Support.FXM.Rules.Contexts;
+  using System;
+  using System.Linq;
 
-    public class ValidateByDomainRuleProcessor : AbstractValidateRequestProcessor<ValidateRequestArgs>
+  public class ValidateByDomainRuleProcessor : AbstractValidateRequestProcessor<ValidateRequestArgs>
+  {
+    public ValidateByDomainRuleProcessor()
     {
-        public ValidateByDomainRuleProcessor()
-        {
-        }
-
-        public ValidateByDomainRuleProcessor(IRuleFactory ruleFactory, IConfigurationFactory configurationFactory, IDomainMatcherRepository domainMatcherRepo) : base(ruleFactory, configurationFactory, domainMatcherRepo)
-        {
-        }
-
-        protected override bool Match(ValidateRequestArgs args, Sitecore.FXM.Rules.Contexts.RequestRuleContext ruleContext, DomainMatcher domainMatcherItem)
-        {
-            Sitecore.Support.FXM.Rules.Contexts.RequestRuleContext context = new Sitecore.Support.FXM.Rules.Contexts.RequestRuleContext(ruleContext.Request);
-            if (!domainMatcherItem.RuleField.Domains.Any<string>())
-            {
-                return ((context.Url != null) && domainMatcherItem.Domain.Equals(context.Url.Host, StringComparison.InvariantCultureIgnoreCase));
-            }
-            base.RuleFactory.ParseRules<Sitecore.FXM.Rules.Contexts.RequestRuleContext>(base.Database.Database, domainMatcherItem.RuleField.InnerField.Value).Run(ruleContext);
-            return ruleContext.IsValid;
-        }
-
-        protected override bool ShouldProcess(ValidateRequestArgs args, Sitecore.FXM.Rules.Contexts.RequestRuleContext ruleContext) => 
-            (args.DomainMatcher == null);
     }
+
+    public ValidateByDomainRuleProcessor(IRuleFactory ruleFactory, IConfigurationFactory configurationFactory, IDomainMatcherRepository domainMatcherRepo) : base(ruleFactory, configurationFactory, domainMatcherRepo)
+    {
+    }
+
+    protected override bool Match(ValidateRequestArgs args, Sitecore.FXM.Rules.Contexts.RequestRuleContext ruleContext, DomainMatcher domainMatcherItem)
+    {
+      bool flag = true;
+      Sitecore.Support.FXM.Rules.Contexts.RequestRuleContext context = new Sitecore.Support.FXM.Rules.Contexts.RequestRuleContext(ruleContext.Request);
+      if (!domainMatcherItem.RuleField.Domains.Any<string>())
+      {
+        return ((context.Url != null) && domainMatcherItem.Domain.Equals(context.Url.Host, StringComparison.InvariantCultureIgnoreCase));
+      }
+      RuleList<Sitecore.Support.FXM.Rules.Contexts.RequestRuleContext> list = base.RuleFactory.ParseRules<Sitecore.Support.FXM.Rules.Contexts.RequestRuleContext>(base.SitecoreContext.Database.Database, domainMatcherItem.RuleField.InnerField.Value);
+      if (list.Count == 0)
+      {
+        return flag;
+      }
+      list.Run(context);
+      return (context.IsValid && flag);
+    }
+
+    protected override bool ShouldProcess(ValidateRequestArgs args, Sitecore.FXM.Rules.Contexts.RequestRuleContext ruleContext) =>
+        (args.DomainMatcher == null);
+  }
 }
